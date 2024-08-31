8e8f1a8393e9459eb600e68155b85eb7755dfac6
JeyAron <aronsule@gmail.com>
2024-08-31 05:31:00 -0700

    a class Square that defines a square by: (based on 0-square.py)

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+""" Define a square class"""
+
+
+class Square:
+    """ representation of a square class"""
+
+    def __init__(self, size):
+        """ instantation with size for our obect
+        initialization
+        """
+        self.__size = size
